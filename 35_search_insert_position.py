e5f524422e99081d6ed562cd88afbf44286fd1bc
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-10-21 13:33:36 +0900

    Update 35_search_insert_position.py
    I have fixed my code.

--- 35_search_insert_position.py
+++ 35_search_insert_position.py
@@ -1,19 +1,20 @@
+%time: O(n)
+%space: O(1)
 from typing import List
 
 
 class Solution:
+
     def searchInsert(self, nums: List[int], target: int) -> int:
         left = 0
-        right = len( nums )
-        flag = 0
-        while flag < 1:
-            if left == right - 1 or left == right:
-                flag += 1
-            now = (right + left ) // 2
-            if nums[ now ] == target:
-                return now
-            elif nums[ now ] < target:
-                left = now
+        right = len(nums)
+        while left < right:
+            mid = (right + left ) // 2
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                right = now
+                right = mid
         return right
