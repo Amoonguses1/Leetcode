fc0648d29f40f9c336f02f25374d10f78461457e
kawai <1019kawai@gmail.com>
2022-10-27 18:13:00 +0900

    Rewrote with binary search

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,25 +1,32 @@
+# time: O(NlogN)
+# space: O(1)
+
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_sorted = sorted(nums)
-        left = 0
-        right = len(nums_sorted) - 1
-        while True:
-            if nums_sorted[left] + nums_sorted[right] == target:
-                break
-            elif nums_sorted[left] + nums_sorted[right] < target:
-                left += 1
-            else:
-                right -= 1
-        indices_list=[]
-        for i in range(len(nums )):
-            if nums[i] == nums_sorted[left]:
-                indices_list.append(i)
-                break
-        for i in range(len(nums )):
-            if nums[i] == nums_sorted[right] and i != indices_list[0]:
-                indices_list.append(i)
-                break
-        return indices_list
+        n = len(nums_sorted)
+        for i in range(n):
+            left = i+1
+            right = n
+            while left < right:
+                mid = (left+right)//2
+                if nums_sorted[i] + nums_sorted[mid] == target:
+                    break
+                elif nums_sorted[i] + nums_sorted[mid] < target:
+                    left = mid+1
+                else:
+                    right = mid
+            indices_list=[]
+            if left != right:
+                for j in range(n):
+                    if nums[j] == nums_sorted[i]:
+                        indices_list.append(j)
+                        break
+                for j in range(n):
+                    if nums[j] == nums_sorted[(left+right)//2] and j != indices_list[0]:
+                        indices_list.append(j)
+                        break
+                return indices_list
