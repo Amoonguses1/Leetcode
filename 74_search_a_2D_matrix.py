7bf420eac9f083eb868843e58595da88a70e2258
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-11-03 19:02:22 +0900

    follow Meguru Style

--- 74_search_a_2D_matrix.py
+++ 74_search_a_2D_matrix.py
@@ -7,26 +7,28 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        up = 0
+        up = -1
         row = len(matrix)
         down = row
-        n = len(matrix[0])
-        while up < down:
+        columns = len(matrix[0])
+        while up < down - 1:
             mid = (up + down) // 2
             if matrix[mid][0] == target:
                 return True
+
             elif matrix[mid][0] < target:
-                up = mid+1
+                up = mid
             else:
                 down = mid
-        left = 0
-        right = n
-        while left < right:
+        left = -1
+        right = columns
+        while left < right - 1:
             mid = (left + right) // 2
-            if matrix[up-1][mid] == target:
+            if matrix[up][mid] == target:
                 return True
-            elif matrix[up-1][mid] < target:
-                left = mid + 1
+
+            elif matrix[up][mid] < target:
+                left = mid
             else:
                 right = mid
         return False
