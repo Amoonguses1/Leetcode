d544d5bcd0a7d141368648bf0cb605e082d421d9
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-11-03 23:40:16 +0900

    I have fixed.

--- 81_search_in_rotated_sorted_array_ii.py
+++ 81_search_in_rotated_sorted_array_ii.py
@@ -8,14 +8,14 @@ from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         nums.sort()
-        left = 0
-        right =len(nums)
-        while left < right:
+        left = -1
+        right = len(nums)
+        while left < right - 1:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return True
             elif nums[mid] < target:
-                left =mid+1
+                left = mid
             else:
                 right = mid
         return False
