a9d87ed8ebd2df84cdfc2d6365c866b4a49dc802
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-11-04 01:24:51 +0900

    I have fixed to follow PEP8.

--- 167_two_sum_ii-input_array_is_sorted_twopointers.py
+++ 167_two_sum_ii-input_array_is_sorted_twopointers.py
@@ -1,19 +1,20 @@
-#Time: O(n)
-#Space: O(1)
+# Time: O(n) n is size of numbers
+# Space: O(1)
 from typing import List
+
 
 class Solution:
 
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left = 0
-        right = len( numbers ) - 1
-        answer_list=[]
+        right = len(numbers) - 1
+        answer_list = []
         while left != right:
             if numbers[left] + numbers[right] == target:
                 answer_list.append(left+1)
                 answer_list.append(right+1)
                 return answer_list
             elif numbers[left] + numbers[right] < target:
-                left+=1
+                left += 1
             else:
-                right-=1
+                right -= 1
