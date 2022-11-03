13cf1b3ce771bd85b0718140886a0002542d0710
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-11-03 19:09:32 +0900

    follow Meguru style

--- 34_find_first_and_last_position_of_element_in_sorted_array.py
+++ 34_find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,36 +1,36 @@
-#time: O(logN) N is the size of nums
-#space O(1)
+# time: O(logN) N is the size of nums
+# space: O(1)
 from typing import List
 
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = 0
+        left = -1
         nums_length = len(nums)
         right = nums_length
         exist_flag = True
-        indices_list = [-1,-1]
-        #find min
-        while left < right:
+        indices_list = [-1, -1]
+        # find min
+        while left < right - 1:
             mid = (left + right) // 2
             if nums[mid] >= target:
                 right = mid
                 if nums[mid] == target:
                     exist_flag = False
             else:
-                left = mid + 1
+                left = mid
         if exist_flag:
             return indices_list
-        
+
         indices_list[0] = right
-        left = 0
+        left = -1
         right = nums_length
-        #find max
-        while left < right:
+        # find max
+        while left < right - 1:
             mid = (left + right) // 2
             if nums[mid] <= target:
-                left = mid + 1
+                left = mid
             else:
                 right = mid
-        indices_list[1] = left - 1
+        indices_list[1] = left
         return indices_list
