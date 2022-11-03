3f4446879da1e83f48e990f7bb1406962a4feadf
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-11-04 01:11:23 +0900

    I added an explanation of a variable.

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,4 +1,4 @@
-# time: O(NlogN)
+# time: O(NlogN) N is size of nums
 # space: O(1)
 
 from typing import List
@@ -7,10 +7,10 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_sorted = sorted(nums)
-        n = len(nums_sorted)
-        for i in range(n):
+        nums_length = len(nums_sorted)
+        for i in range(nums_length):
             left = i
-            right = n
+            right = nums_length
             mid = (left+right)//2
             found_flag = False
             while left < right - 1:
@@ -24,11 +24,11 @@ class Solution:
                 mid = (left+right)//2
             indices_list = []
             if found_flag:
-                for j in range(n):
+                for j in range(nums_length):
                     if nums[j] == nums_sorted[i]:
                         indices_list.append(j)
                         break
-                for j in range(n):
+                for j in range(nums_length):
                     if nums[j] == nums_sorted[mid] and j != indices_list[0]:
                         indices_list.append(j)
                         break
