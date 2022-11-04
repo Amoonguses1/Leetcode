05c0b5d75c2f0929bd3f4f2e76ccad36d83b5c49
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-11-04 22:11:25 +0900

    Add-704_binary_search.

---
+++ 704_binary_search.py
@@ -0,0 +1,19 @@
+# time: O(n)
+# space: O(1)
+from typing import List
+
+
+class Solution:
+
+    def search(self, nums: List[int], target: int) -> int:
+        left = -1
+        right = len(nums)
+        while left < right - 1:
+            now = (left + right)//2
+            if nums[now] == target:
+                return now
+            elif nums[now] > target:
+                right = now
+            else:
+                left = now
+        return -1
