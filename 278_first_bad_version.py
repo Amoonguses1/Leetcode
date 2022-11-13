ffe01b9af00137004fe9269f6554a68305d2e2cf
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-11-14 05:56:44 +0900

    I added error handling and docstring.

--- 278_first_bad_version.py
+++ 278_first_bad_version.py
@@ -1,6 +1,5 @@
 # time: O(logn)
 # space: O(1)
-from typing import List
 
 
 # The isBadVersion API is already defined for you.
@@ -8,10 +7,20 @@ from typing import List
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
+        """Function to find the first version which causes error.
+
+        Args:
+            n(int): integer
+        Return:
+            int: an index of the first version which causes error
+        """
+        if n is None:
+            raise ValueError("error!")
+
         left = 0
         right = n + 1
         while left < right - 1:
-            now = (left + right) // 2
+            now = (left+right) // 2
             if isBadVersion(now):
                 right = now
             else:
