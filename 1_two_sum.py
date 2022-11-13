038a1773f3658810b17d8d454d770585d4d2fe29
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-11-14 06:45:27 +0900

    I added hash map solution. refferring to the repo.

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,11 +1,32 @@
-# time: O(NlogN) N is size of nums
-# space: O(1)
-
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """Function to find indices of the two numbers
+        such that they add up to target.
+        Args:
+            nums(list[int]): list of integer
+            target(int): integer
+        Returns:
+            list[int]: indices of the two numbers
+        Raises:
+            ValueError
+        """
+        """
+        # binary search
+        # n = len(nums)
+        # time: O(nlogn)
+        # space: O(1)
+        if nums is None or target is None:
+            return None
+
+        from_num_to_idx = {}
+        for idx, num in enumerate(nums):
+            if target - num in from_num_to_idx:
+                return [from_num_to_idx[target - num], idx]
+
+            from_num_to_idx[num] = idx
         nums_sorted = sorted(nums)
         nums_length = len(nums_sorted)
         for i in range(nums_length):
@@ -33,3 +54,21 @@ class Solution:
                         indices_list.append(j)
                         break
                 return indices_list
+
+        raise ValueError(f"No combination of nums '{nums}' can be the target '{target}'.")
+        """
+        # hash map
+        # n = len(nums)
+        # time: O(n)
+        # space: O(n)
+        if nums is None or target is None:
+            return None
+
+        from_num_to_idx = {}
+        for idx, num in enumerate(nums):
+            if target - num in from_num_to_idx:
+                return [from_num_to_idx[target - num], idx]
+
+            from_num_to_idx[num] = idx
+
+        raise ValueError(f"No combination of nums '{nums}' can be the target '{target}'.")
