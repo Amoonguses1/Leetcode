5087b799590003459094ca52740f52d39dc850e6
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2022-12-26 02:27:28 +0900

    Add 229 Majority Element II

---
+++ 229_majority_element_ii.py
@@ -0,0 +1,29 @@
+# Time: O(N)
+# Space: O(1)
+# N = len(nums)
+from typing import List
+
+
+def majorityElement(self, nums: List[int]) -> List[int]:
+    """Function to find all the elements that appear more than ⌊ n/3 ⌋ times.
+        Args:
+            nums(List[int]): interger array
+        Returns:
+            List[int]: the elements that appear more than ⌊ n/3 ⌋ times.
+    """
+    if not nums:
+        return []
+    count1, count2, candidate1, candidate2 = 0, 0, 0, 1
+    for n in nums:
+        if n == candidate1:
+            count1 += 1
+        elif n == candidate2:
+            count2 += 1
+        elif count1 == 0:
+            candidate1, count1 = n, 1
+        elif count2 == 0:
+            candidate2, count2 = n, 1
+        else:
+            count1, count2 = count1 - 1, count2 - 1
+    return [n for n in (candidate1, candidate2)
+            if nums.count(n) > len(nums) // 3]
