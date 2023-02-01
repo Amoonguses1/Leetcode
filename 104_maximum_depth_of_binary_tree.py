d3d28d828fcf6d96ef1a14323647f23495313e9a
Amoonguses1 <115347054+Amoonguses1@users.noreply.github.com>
2023-02-01 23:26:27 +0900

    Add 104 Maximum Depth of Binary Tree

---
+++ 104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,43 @@
+# Time: O(N)
+# Space: O(N)
+# N is the number of nodes
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        """Function to return maximum depth
+
+            Args:
+                root(Optional[TreeNode]): binary tree
+
+            Returns:
+                int: the maximum depth
+        """
+        # iterative
+        """
+        stack = list()
+        depth, tmp = 0, 1
+        while root or stack:
+            while root:
+                stack.append([root, tmp])
+                root = root.left
+                tmp += 1
+            root, tmp = stack.pop()
+            depth = max(tmp, depth)
+            root = root.right
+            tmp += 1
+        return depth
+        """
+        # recursive
+        if root is None:
+            return 0
+
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
