c82022fb1cbce0ec46fcc0167d458beadd4772d3
Amoonguses1 <1019kawai@gmail.com>
2023-11-17 15:05:26 +0900

    Add 355 Design Twitter

---
+++ 355_design_twitter.py
@@ -0,0 +1,63 @@
+import collections
+import itertools
+import heapq
+
+
+class Twitter(object):
+
+    def __init__(self):
+        self.timer = itertools.count(step=-1)
+        self.tweets = collections.defaultdict(collections.deque)
+        self.followees = collections.defaultdict(set)
+
+    def postTweet(self, userId, tweetId):
+        """
+        :type userId: int
+        :type tweetId: int
+        :rtype: None
+        """
+        # Time: O(1)
+        # Space: O(1)
+        self.tweets[userId].appendleft(
+            (next(self.timer), tweetId)
+        )
+
+    def getNewsFeed(self, userId):
+        """
+        :type userId: int
+        :rtype: List[int]
+        """
+        # Time: O(N)
+        # Space: O(N)
+        # N is the number of the component of the tweets
+        tweets = heapq.merge(
+            *(self.tweets[u] for u in self.followees[userId] | {userId})
+            )
+        return [tweetId for _, tweetId in itertools.islice(tweets, 10)]
+
+    def follow(self, followerId, followeeId):
+        """
+        :type followerId: int
+        :type followeeId: int
+        :rtype: None
+        """
+        # Time: O(1)
+        # Space: O(1)
+        self.followees[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        """
+        :type followerId: int
+        :type followeeId: int
+        :rtype: None
+        """
+        # Time: O(1)
+        # Space: O(1)
+        self.followees[followerId].discard(followeeId)
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
