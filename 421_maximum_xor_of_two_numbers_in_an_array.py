7b8a1e46f057582031800b11adb19ea3c7dafa01
Amoonguses1 <1019kawai@gmail.com>
2023-12-23 11:14:55 +0900

    Add 421 Maximum XOR of Two Numbers in an Array

---
+++ 421_maximum_xor_of_two_numbers_in_an_array.py
@@ -0,0 +1,14 @@
+# Time: O(N)
+# Space: O(N)
+# N = len(nums)
+from typing import List
+
+
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(32)[::-1]:
+            ans <<= 1
+            prefixes = {num >> i for num in nums}
+            ans += any(ans ^ 1 ^ p in prefixes for p in prefixes)
+        return ans
