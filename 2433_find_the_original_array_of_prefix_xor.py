2078f6d40da42303d9bbad50a82ef4d78e71a9f6
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2024-10-25 20:29:47 +0900

    [Medium/Python] 2433 Find The Original Array of Prefix Xor

---
+++ 2433_find_the_original_array_of_prefix_xor.py
@@ -0,0 +1,11 @@
+# Time: O(N)
+# Space: O(N)
+from typing import List
+
+
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        ans = [pref[0]]
+        for i in range(1, len(pref)):
+            ans.append(pref[i] ^ pref[i-1])
+        return ans
